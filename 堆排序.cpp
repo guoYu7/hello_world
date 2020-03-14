14f7b1164d5024c7cc4c9cdc7efaea913e74e9b0
zj611 <zhangjunbit@163.com>
2020-03-14 11:01:55 +0800

    heapSort

--- 堆排序.cpp
+++ 堆排序.cpp
@@ -7,26 +7,20 @@ using namespace std;
 //最大堆：根结点的键值是所有堆结点键值中最大者，且每个结点的值都比其孩子的值大。
 //最小堆：根结点的键值是所有堆结点键值中最小者，且每个结点的值都比其孩子的值小。
 template<typename T>
-class MaxHeap//定義最大堆
-{
+class MaxHeap{//定義最大堆
 private:
     T* data;
     int count;
     int capacity;
-
-    void shiftUp(int k)
-    {
-        while(k>1&&data[k/2]<data[k])
-        {
+    void shiftUp(int k){
+        while(k>1&&data[k/2]<data[k]){
             swap(data[k/2],data[k]);
             k/=2;
         }
     }
 
-    void shiftDown(int k)
-    {
-        while(2*k<=count)
-        {
+    void shiftDown(int k){
+        while(2*k<=count){
             int j=2*k;
             if(j+1<=count && data[j]<data[j+1])
                 j++;
@@ -38,28 +32,24 @@ private:
     }
 
 public:
-    MaxHeap(int capacity)
-    {
+    MaxHeap(int capacity){
         data = new T[capacity+1];
         count=0;
         this->capacity=capacity;
     }
 
-    ~MaxHeap()
-    {
+    ~MaxHeap(){
         delete[] data;
     }
 
-    void insert(T item)
-    {
+    void insert(T item){
         assert(count+1<=capacity);
         data[count+1] = item;
         count++;
         shiftUp(count);
     }
 
-    T extractMax()
-    {
+    T extractMax(){
         assert(count>0);
         T ret=data[1];
         swap(data[1],data[count]);
@@ -70,41 +60,25 @@ public:
 };
 
 template<typename T>
-void heapsort(vector<T> &v)
-{
+void heapsort(vector<T> &v){
     int n = v.size();
     MaxHeap<T> maxheap=MaxHeap<T>(n);
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         maxheap.insert(v[i]);
     }
-    for(int i=n-1;i>=0;i--)
-    {
+    for(int i=n-1;i>=0;i--){
         v[i]=maxheap.extractMax();
     }
-
 }
 
-int main()
-{
+int main(){
     vector<int> v{1,4,3,2,32,3,2,10,2,12,9,0};
     for(int i=0;i<v.size();i++)
         cout<<v[i]<<" -> ";
     cout<<endl;
-
-
-    //insertsort(v);
-    //quicksort(v);
     heapsort(v);
-
-
     for(int i=0;i<v.size();i++)
         cout<<v[i]<<" -> ";
     cout<<endl;
-
-
 }
-
-
-
 
